a216e1f93b3dcb09783810efc203726ec22958eb
ofsouzap <ofsouzap@gmail.com>
2023-01-29 19:33:16 +0000

    Move int_pow implementation back to mathutil.h

--- mathutil/mathutil.cpp
+++ mathutil/mathutil.cpp
@@ -1,13 +1,1 @@
 #include "mathutil.h"
-
-constexpr int mathutil::int_pow(int b, int n)
-{
-	if (n == 0) return 1;
-	else if (n == 1) return b;
-	else
-	{
-		int rec_res = int_pow(b * b, n / 2);
-		if (n % 2 == 0) return rec_res;
-		else return b * rec_res;
-	}
-}
