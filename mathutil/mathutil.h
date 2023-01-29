a5c7a05330047fb47b68fc09814725e54d3d5d6e
ofsouzap <ofsouzap@gmail.com>
2023-01-29 19:31:08 +0000

    Set CXX minimum to 14

--- mathutil/mathutil.h
+++ mathutil/mathutil.h
@@ -2,15 +2,5 @@
 
 namespace mathutil
 {
-	constexpr int int_pow(int b, int n)
-	{
-		if (n == 0) return 1;
-		else if (n == 1) return b;
-		else
-		{
-			int rec_res = int_pow(b * b, n / 2);
-			if (n % 2 == 0) return rec_res;
-			else return b * rec_res;
-		}
-	}
+	constexpr int int_pow(int b, int n);
 }
