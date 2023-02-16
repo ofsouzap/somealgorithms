b1488bdd2806d7f478a89f5e835cc3207ba62bb9
ofsouzap <ofsouzap@gmail.com>
2023-02-16 13:56:01 +0000

    TreeMap element deletion

--- data_structures/tests/test_tree_map.cpp
+++ data_structures/tests/test_tree_map.cpp
@@ -131,3 +131,291 @@ TEST(TreeMap, GettingNonExistant)
 	ASSERT_THROW(map.get(9), KeyDoesntExistException);
 
 }
+
+TEST(TreeMap, DeleteStartFilled)
+{
+
+	TreeMap<string> map{};
+
+	map.set(5, "five");
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(2, "two");
+	map.set(7, "seven");
+	map.set(1, "one");
+	map.set(4, "four");
+	map.set(3, "three");
+	map.set(6, "six");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	map.remove(5);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_THROW(map.get(5), KeyDoesntExistException);
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+}
+
+TEST(TreeMap, DeleteStartOnlyElement)
+{
+
+	TreeMap<string> map{};
+
+	map.set(0, "zero");
+
+	ASSERT_EQ(map.get(0), "zero");
+
+	map.remove(0);
+
+	ASSERT_THROW(map.get(0), KeyDoesntExistException);
+
+}
+
+TEST(TreeMap, DeleteLeaf)
+{
+
+	TreeMap<string> map{};
+
+	map.set(5, "five");
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(2, "two");
+	map.set(7, "seven");
+	map.set(1, "one");
+	map.set(4, "four");
+	map.set(3, "three");
+	map.set(6, "six");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	map.remove(3);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_THROW(map.get(3), KeyDoesntExistException);
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+}
+
+TEST(TreeMap, DeleteInside)
+{
+
+	TreeMap<string> map{};
+
+	map.set(5, "five");
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(2, "two");
+	map.set(7, "seven");
+	map.set(1, "one");
+	map.set(4, "four");
+	map.set(3, "three");
+	map.set(6, "six");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	map.remove(2);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_THROW(map.get(2), KeyDoesntExistException);
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+}
+
+TEST(TreeMap, DeleteEnd)
+{
+
+	TreeMap<string> map{};
+
+	map.set(5, "five");
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(2, "two");
+	map.set(7, "seven");
+	map.set(1, "one");
+	map.set(4, "four");
+	map.set(3, "three");
+	map.set(6, "six");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	map.remove(8);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_THROW(map.get(8), KeyDoesntExistException);
+
+}
+
+TEST(TreeMap, DeleteNonExistant)
+{
+
+	TreeMap<string> map{};
+
+	map.set(5, "five");
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(2, "two");
+	map.set(7, "seven");
+	map.set(1, "one");
+	map.set(4, "four");
+	map.set(3, "three");
+	map.set(6, "six");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	ASSERT_THROW(map.remove(20), KeyDoesntExistException);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+}
+
+TEST(TreeMap, DeleteNoSuccessorRoot)
+{
+
+	TreeMap<string> map{};
+
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(1, "one");
+	map.set(2, "two");
+	map.set(3, "three");
+	map.set(4, "four");
+	map.set(5, "five");
+	map.set(6, "six");
+	map.set(7, "seven");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	map.remove(8);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_THROW(map.get(8), KeyDoesntExistException);
+
+}
+
+TEST(TreeMap, DeleteNoSuccessorInside)
+{
+
+	TreeMap<string> map{};
+
+	map.set(5, "five");
+	map.set(8, "eight");
+	map.set(0, "zero");
+	map.set(2, "two");
+	map.set(7, "seven");
+	map.set(1, "one");
+	map.set(4, "four");
+	map.set(3, "three");
+	map.set(6, "six");
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_EQ(map.get(8), "eight");
+
+	map.remove(8);
+
+	ASSERT_EQ(map.get(0), "zero");
+	ASSERT_EQ(map.get(1), "one");
+	ASSERT_EQ(map.get(2), "two");
+	ASSERT_EQ(map.get(3), "three");
+	ASSERT_EQ(map.get(4), "four");
+	ASSERT_EQ(map.get(5), "five");
+	ASSERT_EQ(map.get(6), "six");
+	ASSERT_EQ(map.get(7), "seven");
+	ASSERT_THROW(map.get(8), KeyDoesntExistException);
+
+}
--- data_structures/tree_map.h
+++ data_structures/tree_map.h
@@ -17,6 +17,118 @@ namespace data_structures
 
 		Node* root;
 
+		void swap_nodes(Node* const n1, Node* const n2)
+		{
+
+			Node* n1_parent = n1->parent;
+
+			bool n1_is_left_child = true;
+			if (n1_parent != nullptr)
+				n1_is_left_child = n1_parent->left == n1;
+
+			Node* n2_parent = n2->parent;
+
+			bool n2_is_left_child = true;
+			if (n2_parent != nullptr)
+				n2_is_left_child = n2_parent->left == n2;
+
+			// Change tree root if needed
+
+			if (root == n1)
+				root = n2;
+			else if (root == n2)
+				root = n1;
+
+			// Change parents' children pointers
+
+			if (n1_parent != nullptr)
+			{
+
+				// Set n1's new child to be n2
+
+				if (n1_is_left_child)
+					n1_parent->left = n2;
+				else
+					n1_parent->right = n2;
+
+			}
+
+			if (n2_parent != nullptr)
+			{
+
+				// Set n2's new child to be n1
+
+				if (n2_is_left_child)
+					n2_parent->left = n1;
+				else
+					n2_parent->right = n1;
+
+			}
+
+			// Change nodes' parent pointers
+
+			n1->parent = n2_parent;
+			n2->parent = n1_parent;
+
+			// Swap children
+
+			Node* n1_left = n1->left;
+			Node* n1_right = n1->right;
+
+			n1->left = n2->left;
+			n1->right = n2->right;
+
+			n2->left = n1_left;
+			n2->right = n1_right;
+
+		}
+
+		Node* succ(const Node* const node) const
+		{
+
+			if (node->right == nullptr)
+			{
+				return nullptr;
+			}
+			else
+			{
+				
+				Node* curr = node->right;
+
+				while (curr->left != nullptr)
+					curr = curr->left;
+
+				return curr;
+
+			}
+
+		}
+
+		Node* get_node(const K_t key) const
+		{
+
+			Node* curr = root;
+
+			while (curr != nullptr)
+			{
+				if (key == curr->key)
+				{
+					return curr;
+				}
+				else if (key < curr->key)
+				{
+					curr = curr->left;
+				}
+				else
+				{
+					curr = curr->right;
+				}
+			}
+
+			throw KeyDoesntExistException();
+
+		}
+
 	public:
 
 		TreeMap()
@@ -75,28 +187,90 @@ namespace data_structures
 
 		}
 
-		V_t get(K_t key) const
+		V_t get(const K_t key) const
+		{
+			return get_node(key)->value;
+		}
+
+		void remove(const K_t key)
 		{
 
-			Node* curr = root;
+			Node* node = get_node(key);
+			Node* n_succ = succ(node);
 
-			while (curr != nullptr)
+			if (node->is_leaf())
 			{
-				if (key == curr->key)
+
+				// If node is leaf then it can just be deleted
+
+				if (root == node)
 				{
-					return curr->value;
-				}
-				else if (key < curr->key)
-				{
-					curr = curr->left;
+
+					root = nullptr;
+
 				}
 				else
 				{
-					curr = curr->right;
+
+					// Remove node as parent's child
+
+					bool node_is_left_child = node->parent->left == node;
+
+					if (node_is_left_child)
+						node->parent->left = nullptr;
+					else
+						node->parent->right = nullptr;
+
 				}
+
+			}
+			else if (n_succ == nullptr)
+			{
+
+				// If the node has no successor then it can only have a left child
+
+				if (root == node)
+				{
+
+					root = node->left;
+
+				}
+				else
+				{
+
+					// Swap the node for its left subtree
+
+					bool node_is_left_child = node->parent->left == node;
+
+					if (node_is_left_child)
+						node->parent->left = node->left;
+					else
+						node->parent->right = node->left;
+
+				}
+
+			}
+			else
+			{
+
+				// Replace node with its successor
+
+				swap_nodes(node, n_succ);
+
+				// Remove node from tree
+
+				bool node_is_left_child = node->parent->left == node;
+
+				if (node_is_left_child)
+					node->parent->left = nullptr;
+				else
+					node->parent->right = nullptr;
+
 			}
 
-			throw KeyDoesntExistException();
+			// Delete node
+
+			delete node;
 
 		}
 
@@ -116,6 +290,11 @@ namespace data_structures
 		{
 			left = nullptr;
 			right = nullptr;
+		}
+
+		bool is_leaf() const
+		{
+			return left == nullptr && right == nullptr;
 		}
 
 	};
